1485e73ed962c7ffb6953cfca0c8e9a5641efbb2
David Lee <913305994@qq.com>
2016-11-30 21:52:08 +0800

    Add files via upload

--- No2006.java
+++ No2006.java
@@ -7,9 +7,9 @@ public class No2006 {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		Scanner sc = new Scanner(System.in);
-		int result = 1;
 		while(sc.hasNextInt()){
 			int n = sc.nextInt();
+			int result = 1;
 			for(int i=0;i<n;i++){
 				int input = sc.nextInt();
 				if(input%2 == 1){
