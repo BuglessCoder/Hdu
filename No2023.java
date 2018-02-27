a784d598e305e21b2f941e504c7adad3a6475ed2
David Lee <913305994@qq.com>
2018-02-27 20:30:55 +0800

    补这道一直没有AC的题，检查了好几遍算法，最后发现再算count的时候没有看到题目中的大于等于...

--- No2023.java
+++ No2023.java
@@ -1,68 +1,66 @@
 package hdu;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class No2023 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextInt()){
-			int m = sc.nextInt();
+		while(sc.hasNext()){
 			int n = sc.nextInt();
-			DecimalFormat df = new DecimalFormat("######0.00"); 
-			double[] avgN = new double[m];
-			double[] avgM = new double[n];
-			int[][] a = new int[m][n];
-			for(int i=0;i<m;i++){
-				for(int j=0;j<n;j++){
-					a[i][j] = sc.nextInt();
-				}
-			}
+			int m = sc.nextInt();
+			double[] avgN = new double[n];	//n个学生的平均成绩（每行的平均）
+			double[] avgM = new double[m];	//m门课程的平均成绩（每列的平均）
+			int[][] a = new int[n][m];
 			double sumN = 0;
 			double sumM = 0;
-			for(int i=0;i<m;i++){
+			for(int i=0;i<n;i++){
 				sumN = 0;
-				for(int j=0;j<n;j++){
+				for(int j=0;j<m;j++){
+					a[i][j] = sc.nextInt();
 					sumN+=a[i][j];
-				}
-				avgN[i] = (double)sumN/n;
+				} 
+				avgN[i] = (double)sumN/m;
 			}
-			for(int j=0;j<n;j++){
+
+			for(int j=0;j<m;j++){
 				sumM = 0;
-				for(int i=0;i<m;i++){
+				for(int i=0;i<n;i++){
 					sumM+=a[i][j];
 				}
-				avgM[j] = (double)sumM/m;
+				avgM[j] = (double)sumM/n;
 			}
-			for(int i=0;i<m;i++){
-				if(i!=m-1){
-					System.out.print(df.format(avgN[i]) + " ");
+			
+			//输出
+			for(int i=0;i<n;i++){
+				if(i!=n-1){
+					System.out.printf("%.2f ",avgN[i]);
 				}
 				else{
-					System.out.print(df.format(avgN[i]));
+					System.out.printf("%.2f",avgN[i]);
+					System.out.println();
 				}
 			}
-			System.out.println();
-			for(int j=0;j<n;j++){
-				if(j!=n-1){
-					System.out.print(df.format(avgM[j]) + " ");
+			
+			for(int j=0;j<m;j++){
+				if(j!=m-1){
+					System.out.printf("%.2f ",avgM[j]);
 				}
 				else{
-					System.out.print(df.format(avgM[j]));
-				}
-				
+					System.out.printf("%.2f",avgM[j]);
+					System.out.println();
+				}			
 			}
-			System.out.println();
+			
 			int sum = 0;
 			int count = 0;
-			for(int i=0;i<m;i++){
+			for(int i=0;i<n;i++){
 				sum = 0;
-				for(int j=0;j<n;j++){
-					if(a[i][j]>avgM[j]){
+				for(int j=0;j<m;j++){
+					if(a[i][j]>=avgM[j]){
 						sum++;
 					}
 				}
-				if(sum == n){
+				if(sum == m){
 					count++;
 				}
 			}
@@ -70,5 +68,4 @@ public class No2023 {
 			System.out.println();
 		}
 	}
-
 }
